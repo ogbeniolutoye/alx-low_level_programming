404721d31607eed2e851572adc5c7a0ad6688d0b
ogbeniolutoye <adewumiolutoye@gmail.com>
2022-10-18 21:32:19 -0700

    Zero putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
-**main - Prints Holberton as a message.
-*
-**Return: Always 0 (Success)
+ * main - Prints Holberton as a message
+ * 
+ * Return: Always 0 (Success)
 **/
 int main(void)
 {
