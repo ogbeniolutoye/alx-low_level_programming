6ae161a0ebf17f0dc506bec94c9b001f559e819a
ogbeniolutoye <adewumiolutoye@gmail.com>
2022-10-22 17:19:17 -0700

    Advanced tasks

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,6 +4,7 @@ extern int _isupper(int c);
 extern int _isdigit(int c);
 extern int mul(int a, int b);
 extern void print_numbers(void);
+extern int _putchar(char);
 extern void print_most_numbers(void);
 extern void more_numbers(void);
 extern void print_line(int n);
@@ -11,4 +12,8 @@ extern void print_diagonal(int n);
 extern void print_square(int size);
 extern void print_triangle(int size);
 extern void print_number(int n);
+extern void times_table(void);
+extern int add(int, int);
+extern void print_to_98(int n);
+extern void print_times_table(int n);
 #endif
